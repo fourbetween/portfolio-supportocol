b0d65552dc57f2fff34453c354d7b6bc37eeb4ec
hikaru ishiguro <rukahirogushii0320@gmail.com>
2025-11-21 14:09:31 +0000

    RuleListPresenterコンポーネントを新規作成し、関連するストーリーとテストケースを追加しました。また、インポート文を更新しました。

--- view/src/import.ts
+++ view/src/import.ts
@@ -7,6 +7,7 @@ import "./component/container/page/rules";
 import "./component/presenter/header/main";
 import "./component/presenter/list/discussion";
 import "./component/presenter/list/project";
+import "./component/presenter/list/rule";
 import "./component/presenter/popup/base";
 import "./component/presenter/popup/project/add_discussion";
 import "./component/presenter/popup/project/create";
