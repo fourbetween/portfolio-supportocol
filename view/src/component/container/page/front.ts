0d9bf37d94e1ed32f6560e160ed58ec5b9e3dbf9
hikaru ishiguro <rukahirogushii0320@gmail.com>
2025-11-21 07:32:32 +0000

    不要なコードをコメントアウトし、フロントページのレンダリングを簡素化しました

--- view/src/component/container/page/front.ts
+++ view/src/component/container/page/front.ts
@@ -1,8 +1,5 @@
-import { Task } from "@lit/task";
 import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import { client } from "../../../api/client";
-import { accountMethods } from "../../../model/account";
 import { baseStyle } from "../../../style/base";
 
 @customElement("front-page-container")
@@ -10,28 +7,22 @@ export class FrontPageContainer extends LitElement {
   @property({ type: Boolean })
   isLoggedIn = false;
 
-  private workbooksTask = new Task(this, {
-    task: async ([]) => {
-      const { data, error } = await client.GET("/workbooks", {
-        headers: await accountMethods.authHeader(),
-      });
-      if (error) {
-        throw new Error(error.message);
-      }
-      return data;
-    },
-    args: () => [],
-  });
+  // private workbooksTask = new Task(this, {
+  //   task: async ([]) => {
+  //     const { data, error } = await client.GET("/workbooks", {
+  //       headers: await accountMethods.authHeader(),
+  //     });
+  //     if (error) {
+  //       throw new Error(error.message);
+  //     }
+  //     return data;
+  //   },
+  //   args: () => [],
+  // });
 
   render() {
     return html`
-      ${this.workbooksTask.render({
-        complete: (workbooks) => html`
-          <workbook-list-presenter
-            .workbooks=${workbooks}
-          ></workbook-list-presenter>
-        `,
-      })}
+      front page
     `;
   }
 
