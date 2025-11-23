711243af21fdda868a9534ac39362387a41954c9
hikaru ishiguro <rukahirogushii0320@gmail.com>
2025-11-23 06:02:36 +0000

    プロジェクトタスクを追加し、ダッシュボードにプロジェクトリストを表示するように変更しました

--- view/src/component/container/page/dashboard.ts
+++ view/src/component/container/page/dashboard.ts
@@ -1,5 +1,8 @@
+import { Task } from "@lit/task";
 import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
+import { client } from "../../../api/client";
+import { accountMethods } from "../../../model/account";
 import { baseStyle } from "../../../style/base";
 
 @customElement("dashboard-page-container")
@@ -7,11 +10,34 @@ export class DashboardPageContainer extends LitElement {
   @property({ type: Boolean })
   isLoggedIn = false;
 
+  private projectsTask = new Task(this, {
+    task: async ([]) => {
+      const { data, error } = await client.GET("/projects", {
+        headers: await accountMethods.authHeader(),
+      });
+      if (error) {
+        throw new Error(error.message);
+      }
+      return data;
+    },
+    args: () => [],
+  });
+
   render() {
     return html`
       <div class="container">
         <div class="side">
-          <project-list-presenter .projects=${[]}></project-list-presenter>
+          ${this.projectsTask.render({
+            complete: (projects) => html`
+              <project-list-presenter
+                .projects=${projects}
+              ></project-list-presenter>
+            `,
+            error: (e) =>
+              html`
+                <p>Error: ${e}</p>
+              `,
+          })}
         </div>
         <div class="main">
           <discussion-list-presenter
