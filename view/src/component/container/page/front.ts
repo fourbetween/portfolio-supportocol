66a33d623c8b38ec287a8935127a9c1ef83cdb5f
hikaru ishiguro <rukahirogushii0320@gmail.com>
2025-11-25 16:22:39 +0000

    不要なルーティング関連のコードを削除し、ダッシュボードへのリンクをシンプルなハイパーリンクに変更

--- view/src/component/container/page/front.ts
+++ view/src/component/container/page/front.ts
@@ -1,46 +1,21 @@
-import type { Router } from "@lit-labs/router";
-import { consume } from "@lit/context";
 import { LitElement, css, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import { routerContext } from "../../../context/router";
-import { navigate } from "../../../routes";
 import { baseStyle } from "../../../style/base";
 
 @customElement("front-page-container")
 export class FrontPageContainer extends LitElement {
-  @consume({ context: routerContext })
-  private router!: Router;
-
   @property({ type: Boolean })
   isLoggedIn = false;
-
-  // private workbooksTask = new Task(this, {
-  //   task: async ([]) => {
-  //     const { data, error } = await client.GET("/workbooks", {
-  //       headers: await accountMethods.authHeader(),
-  //     });
-  //     if (error) {
-  //       throw new Error(error.message);
-  //     }
-  //     return data;
-  //   },
-  //   args: () => [],
-  // });
 
   render() {
     return html`
       <ul>
         <li>
-          <a @click=${this.handleNavigateToDashboard}>Go to Dashboard</a>
+          <a href="/dashboard">Go to Dashboard</a>
         </li>
       </ul>
     `;
   }
-
-  private handleNavigateToDashboard = async (e: Event) => {
-    e.preventDefault();
-    await navigate(this.router, "dashboard");
-  };
 
   static styles = [baseStyle, css``];
 }
