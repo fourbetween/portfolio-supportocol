1d663999da4e6bb1631eb9d5b80a26fd4a5e29e7
hikaru ishiguro <rukahirogushii0320@gmail.com>
2025-11-25 12:36:00 +0000

    ホームページプレゼンター関連のファイルを削除し、ポップアッププレゼンターの新規作成とテストを追加

---
+++ view/src/component/presenter/popup/base.test.ts
@@ -0,0 +1,22 @@
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import type { BasePopupPresenter } from "./base";
+
+describe("BasePopupPresenter", async () => {
+  let elem: BasePopupPresenter;
+
+  beforeEach(() => {
+    elem = document.createElement("base-popup-presenter") as BasePopupPresenter;
+    document.body.appendChild(elem);
+  });
+
+  afterEach(() => {
+    elem.remove();
+  });
+
+  it("open()メソッドを呼ぶとダイアログが表示されること", async () => {
+    elem.open();
+    await elem.updateComplete;
+    const dialog = elem.renderRoot.querySelector("dialog");
+    expect(dialog?.open).toBe(true);
+  });
+});
--- view/src/import.ts
+++ view/src/import.ts
@@ -1,3 +1,3 @@
 import "./component/container/layout/main";
 import "./component/container/page/front";
-import "./component/presenter/page/home";
+import "./component/presenter/popup/base";
