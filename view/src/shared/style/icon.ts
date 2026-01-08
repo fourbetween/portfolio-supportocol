54833b5abeb50e3b27e0e684600358ab807b569f
hikaru ishiguro <rukahirogushii0320@gmail.com>
2026-01-08 12:38:19 +0000

    comment-itemのアイコンを大きくする Fixes #233

--- view/src/shared/style/icon.ts
+++ view/src/shared/style/icon.ts
@@ -3,6 +3,7 @@ import { type CSSResultGroup, css } from "lit";
 export const iconStyle: CSSResultGroup = [
   css`
     .material-symbols-outlined {
+      font-size: 18px;
       font-family: "Material Symbols Outlined";
       font-variation-settings: "FILL" 0, "wght" 400, "GRAD" 0, "opsz" 24;
     }
