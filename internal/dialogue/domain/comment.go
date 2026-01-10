a6bbe2cbeafc65ff4d565edcc0a585a4ef324ff1
hikaru ishiguro <rukahirogushii0320@gmail.com>
2026-01-10 07:05:04 +0000

    コメントおよびディスカッションのドメインモデルとファクトリを追加し、リポジトリインターフェースを定義しました。

---
+++ internal/dialogue/domain/comment.go
@@ -0,0 +1,72 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/fourbetween/app-supportocol/internal/pkg/apperr"
+)
+
+type Comment struct {
+	id              string
+	discussionID    string
+	parentCommentID *string
+	commentType     string
+	content         string
+	status          CommentStatus
+	createdBy       string
+	createdAt       time.Time
+}
+
+func (c *Comment) ID() string {
+	return c.id
+}
+
+func (c *Comment) DiscussionID() string {
+	return c.discussionID
+}
+
+func (c *Comment) ParentCommentID() *string {
+	return c.parentCommentID
+}
+
+func (c *Comment) CommentType() string {
+	return c.commentType
+}
+
+func (c *Comment) Content() string {
+	return c.content
+}
+
+func (c *Comment) Status() CommentStatus {
+	return c.status
+}
+
+func (c *Comment) CreatedBy() string {
+	return c.createdBy
+}
+
+func (c *Comment) CreatedAt() time.Time {
+	return c.createdAt
+}
+
+type UpdateCommentParams struct {
+	CommentType string
+	Content     string
+}
+
+func (c *Comment) Update(params UpdateCommentParams) error {
+	c.commentType = params.CommentType
+	c.content = params.Content
+	return nil
+}
+
+func (c *Comment) UpdateStatus(status CommentStatus) error {
+	if !status.IsValid() {
+		return apperr.ErrInvalidArgument
+	}
+	if c.status == CommentStatusActive && status == CommentStatusProposed {
+		return apperr.ErrInvalidArgument
+	}
+	c.status = status
+	return nil
+}
---
+++ internal/dialogue/domain/discussion.go
@@ -0,0 +1,28 @@
+package domain
+
+import (
+	"time"
+)
+
+type Discussion struct {
+	id        string
+	theme     string
+	createdBy string
+	createdAt time.Time
+}
+
+func (d *Discussion) ID() string {
+	return d.id
+}
+
+func (d *Discussion) Theme() string {
+	return d.theme
+}
+
+func (d *Discussion) CreatedBy() string {
+	return d.createdBy
+}
+
+func (d *Discussion) CreatedAt() time.Time {
+	return d.createdAt
+}
