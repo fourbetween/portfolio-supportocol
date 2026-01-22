d2e716047802c1a034218758dd605aa79c275a40
hikaru ishiguro <rukahirogushii0320@gmail.com>
2026-01-22 13:37:36 +0000

    ワークスペースプロジェクト選択ウィジェットコンポーネントを追加しました

---
+++ view/src/feature/workspace/component/project-select-widget.ts
@@ -0,0 +1,63 @@
+import { consume } from "@lit/context";
+import { Task } from "@lit/task";
+import { LitElement, css, html } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
+import { userContext } from "../../../app/context/user";
+import type { User } from "../../../app/model/user";
+import { baseStyle } from "../../../shared/style/base";
+import type { Project } from "../model/project";
+import { projectRepository } from "../repository/project-repository";
+import "../ui/project-select/project-select";
+
+@customElement("workspace-project-select-widget")
+export class WorkspaceProjectSelectWidget extends LitElement {
+  @property({ type: String })
+  selectedProjectId = "";
+
+  @state()
+  private projects: Project[] = [];
+
+  @consume({ context: userContext, subscribe: true })
+  @state()
+  private user?: User;
+
+  constructor() {
+    super();
+
+    new Task(this, {
+      task: async () => {
+        if (!this.user) {
+          return [] as Project[];
+        }
+        return projectRepository.list("personal-" + this.user.id);
+      },
+      args: () => [],
+      onComplete: (projects: Project[]) => {
+        this.projects = projects;
+      },
+    });
+  }
+
+  render() {
+    return html`
+      <div class="widget">
+        <workspace-project-select
+          .projects=${this.projects}
+          .selectedProjectId=${this.selectedProjectId}
+        ></workspace-project-select>
+      </div>
+    `;
+  }
+
+  static styles = [
+    baseStyle,
+    css`
+      .widget {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        width: 100%;
+      }
+    `,
+  ];
+}
