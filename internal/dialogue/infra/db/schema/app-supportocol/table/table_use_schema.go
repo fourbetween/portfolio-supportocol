6b15042a0dc9791e698f63bf6c7b9d9a6a844890
hikaru ishiguro <rukahirogushii0320@gmail.com>
2026-01-31 04:36:21 +0000

    コメントと課題の管理機能を追加し、データベーススキーマを更新

--- internal/dialogue/infra/db/schema/app-supportocol/table/table_use_schema.go
+++ internal/dialogue/infra/db/schema/app-supportocol/table/table_use_schema.go
@@ -10,7 +10,9 @@ package table
 // UseSchema sets a new schema name for all generated table SQL builder types. It is recommended to invoke
 // this method only once at the beginning of the program.
 func UseSchema(schema string) {
+	CommentIssues = CommentIssues.FromSchema(schema)
 	Comments = Comments.FromSchema(schema)
 	DialogueSettings = DialogueSettings.FromSchema(schema)
 	Discussions = Discussions.FromSchema(schema)
+	Issues = Issues.FromSchema(schema)
 }
