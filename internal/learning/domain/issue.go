7410ff17e6bc44837c7a2f4ecb5e83f6b2a6b3de
hikaru ishiguro <rukahirogushii0320@gmail.com>
2026-02-02 07:22:06 +0000

    Issue構造体のバリデーションを追加し、必須フィールドのチェックを実装しました。また、IssueStatusのバリデーションメソッドを強化しました。

--- internal/learning/domain/issue.go
+++ internal/learning/domain/issue.go
@@ -1,5 +1,11 @@
 package domain
 
+import (
+	"fmt"
+
+	"github.com/fourbetween/app-supportocol/internal/pkg/apperr"
+)
+
 type Issue struct {
 	id          string
 	issueType   string
@@ -24,5 +30,17 @@ func (i *Issue) Status() IssueStatus {
 }
 
 func (i *Issue) Validate() error {
+	if i.id == "" {
+		return fmt.Errorf("%w: issue id is required", apperr.ErrInvalidArgument)
+	}
+	if i.issueType == "" {
+		return fmt.Errorf("%w: issue type is required", apperr.ErrInvalidArgument)
+	}
+	if i.description == "" {
+		return fmt.Errorf("%w: issue description is required", apperr.ErrInvalidArgument)
+	}
+	if err := i.status.Validate(); err != nil {
+		return fmt.Errorf("%w: invalid issue status: %s", err, i.status)
+	}
 	return nil
 }
