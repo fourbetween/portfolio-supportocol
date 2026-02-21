f2ca1d32d217cbe714846cf4c8fcf93f6f75a418
hikaru ishiguro <rukahirogushii0320@gmail.com>
2026-02-21 14:15:09 +0000

    無料プランの月間AI制限を0から20に更新しました

--- cmd/db/seed/main.go
+++ cmd/db/seed/main.go
@@ -15,7 +15,7 @@ var plans = []domain.Plan{
 		ID:             "4c4ca77d-4a51-463e-9345-da27010cb450",
 		Name:           "Free",
 		Description:    "Free plan for personal use.",
-		MonthlyAILimit: 0,
+		MonthlyAILimit: 20,
 	},
 	{
 		ID:             "cbe0d635-c20c-4c06-9933-1979fac9f5af",
